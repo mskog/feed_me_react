83b5eaa48fe72a42056852f014494e094b63f24f
Magnus Skog <magnus.m.skog@gmail.com>
2016-03-21 20:50:01 +0100

    Sort of nicer feed entries

--- src/components/feed_entry.jsx
+++ src/components/feed_entry.jsx
@@ -1,10 +1,23 @@
 import React, { PropTypes } from 'react';
 import ListItem from 'material-ui/lib/lists/list-item';
+import {Card, CardHeader, CardText} from 'material-ui/lib/card';
 
 class FeedEntry extends React.Component {
   render() {
+    var image = "";
+
+    if(this.props.image !== null){
+      image = <img src={`http://thumbs.picyo.me/200x200/smart/${this.props.image}`} />;
+    }
+
     return (
-      <ListItem primaryText={this.props.title} />
+      <Card>
+        <CardHeader title={this.props.title}/>
+        <CardText>
+          {image}
+          {this.props.summary}
+        </CardText>
+      </Card>
     );
   }
 }
