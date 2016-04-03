bd6d7436aa7def8a7822ce1a578ffec13c95166f
Magnus Skog <magnus.m.skog@gmail.com>
2016-04-03 12:02:48 +0200

    Pagination, sort of

--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -1,30 +1,34 @@
 import React, { PropTypes } from 'react';
 import { connect, PromiseState } from 'react-refetch'
-import {Preloader} from 'react-materialize';
+import {Preloader, Button} from 'react-materialize';
+import { router, browserHistory, Link } from 'react-router';
 
 import FeedEntriesList from './feed_entries_list';
 
 class Feed extends React.Component {
+
+  static contextTypes = {
+    router: PropTypes.object.isRequired
+  };
+
+  currentPage(){
+    let page = this.props.location.query.page;
+    return page !== undefined ? page : 1;
+  }
+
+  nextPage(){
+    let currentPath = this.props.location.pathname;
+    browserHistory.push(`${currentPath}?page=${parseInt(this.currentPage())+1}`);
+  }
+
   render() {
-    const {entriesFetch} = this.props;
-    var entries = ';'
-
-    if (entriesFetch.fulfilled){
-      entries = <FeedEntriesList entries={entriesFetch.value['user_feed_entries']} />;
-    } else{
-      entries = <div className="center-align"><Preloader size="small" /></div>;
-    }
-
     return (
       <div>
-        {entries}
+        <Button onClick={this.nextPage.bind(this)}>More...</Button>
+        <FeedEntriesList id={this.props.params.id} page={this.currentPage()}/>
       </div>
     );
   }
 }
 
 export default Feed;
-
-export default connect(props => ({
-  entriesFetch: `https://feedme.mskog.com/api/v1/feeds/${props.params.id}/entries.json?user_email=mrcheese0@gmail.com&user_token=4Mxa-sFZ26RiMttynmQ5`
-}))(Feed)
--- src/components/feed_entries_list.jsx
+++ src/components/feed_entries_list.jsx
@@ -1,12 +1,21 @@
 import React, { PropTypes } from 'react';
 import FeedEntry from './feed_entry';
+import { connect, PromiseState } from 'react-refetch';
+import {Preloader} from 'react-materialize';
 
 class FeedEntriesList extends React.Component {
   render() {
-    let entries = this.props.entries.map(function(entry, index) {
-      return (
-        <FeedEntry {...entry} key={entry.id} />
-    )});
+    const {entriesFetch} = this.props;
+    var entries = ';'
+
+    if (entriesFetch.fulfilled){
+      entries = entriesFetch.value['user_feed_entries'].map(function(entry, index) {
+        return (
+          <FeedEntry {...entry} key={entry.id} />
+      )});
+    } else{
+      entries = <div className="center-align"><Preloader size="small" /></div>;
+    }
 
     return(
       <div>
@@ -17,3 +26,7 @@ class FeedEntriesList extends React.Component {
 }
 
 export default FeedEntriesList;
+
+export default connect(props => ({
+  entriesFetch: `https://feedme.mskog.com/api/v1/feeds/${props.id}/entries.json?page=${props.page}&user_email=mrcheese0@gmail.com&user_token=4Mxa-sFZ26RiMttynmQ5`
+}))(FeedEntriesList)
